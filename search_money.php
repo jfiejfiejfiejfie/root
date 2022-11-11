46bfe87acf3f5740bbf3e0e152cc9e015362f713
jfiejfiejfiejfie <yuttarishin@gmail.com>
2022-11-11 17:19:05 +0900

    極！源！炎！舞！！！！！

--- search_money.php
+++ search_money.php
@@ -18,13 +18,7 @@ if (!cken($_POST)){
 <body>
 <audio id="audio"></audio>
 <div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.5&appId=643231655816289";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
+
   <?php
     // 簡単なエラー処理
     $errors = [];
