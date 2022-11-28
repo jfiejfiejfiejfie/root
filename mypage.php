86e245543d6aa81dce4813eb5bc329f984ac4082
wakkasan <fki2166275@stu.o-hara.ac.jp>
2022-11-28 11:23:26 +0900

    実験したいので

--- mypage.php
+++ mypage.php
@@ -25,6 +25,7 @@ require_once('db_connect.php');
 
       <?php
       echo '<br>';
+      echo 'あなたのIPアドレスは'.$_SERVER['REMOTE_ADDR'];
     if (!isset($_SESSION["loggedin"])) {
       echo "<h2>この機能を利用するにはログインしてください。</h2>";
       echo "<a href='login.php' class='btn btn-danger'>ログイン</a>";
