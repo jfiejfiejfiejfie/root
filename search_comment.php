1c9f1d82b0554929b9d00c4eb4f081e372746792
jfiejfiejfiejfie <yuttarishin@gmail.com>
2022-12-02 11:19:21 +0900

    掲示板の検索

--- search_comment.php
+++ search_comment.php
@@ -1,51 +1,191 @@
 <?php
-session_start(); 
+session_start();
 require_once('../lib/util.php');
-$myURL='search_comment.php';
-$gobackURL = "keijiban.php";
-require_once "db_connect.php";
-define('MAX','5');
-// 文字エンコードの検証
-if (!cken($_POST)){
-  header("Location:{$gobackURL}");
-  exit();
+if (!isset($_SESSION["loggedin"])) {
+  header('Location:login.php');
 }
-
-// nameが未設定、空のときはエラー
-if (empty($_POST)){
-    header("Location:searchform.html");
-    exit();
-  } else if(!isset($_POST["message"])||($_POST["message"]==="")){
-    header("Location:{$gobackURL}");
-    exit();
+// if ("location:login.php")
+//     ;
+require_once "db_connect.php";
+require_once('checked.php');
+require_once "db_connect.php";
+$myURL = 'add_db.php';
+$gobackURL = 'index.php';
+$point = 0;
+if (isset($_POST["kind"])) {
+  require_once('insert.php');
+}
+?>
+<?php 
+//エスケープ処理やデータチェックを行う関数のファイルの読み込み
+require 'functionZ.php';
+ 
+//POSTされたデータを変数に格納（値の初期化とデータの整形：前後にあるホワイトスペースを削除）
+$name = trim( filter_input(INPUT_POST, 'name') );
+$email = trim( filter_input(INPUT_POST, 'email') );
+$tel = trim( filter_input(INPUT_POST, 'tel') );
+$subject = trim( filter_input(INPUT_POST, 'subject'));
+$body = trim( filter_input(INPUT_POST, 'body') );
+$image = trim( filter_input(INPUT_POST, 'image') );
+ 
+//送信ボタンが押された場合の処理
+if (isset($_POST['submitted'])) {
+ 
+  //POSTされたデータをチェック  
+  $_POST = checkInput( $_POST ); 
+ 
+  //エラーメッセージを保存する配列の初期化
+  $error = array();
+  
+  //値の検証
+  if ( $name == '' ) {
+    $error['name'] = '*お名前は必須項目です。';
+    //制御文字でないことと文字数をチェック
+  } else if ( preg_match( '/\A[[:^cntrl:]]{1,30}\z/u', $name ) == 0 ) {
+    $error['name'] = '*お名前は30文字以内でお願いします。';
   }
-
+  if ( $email == '' ) {
+    $error['email'] = '*メールアドレスは必須です。';
+  } else { //メールアドレスを正規表現でチェック
+    $pattern = '/\A([a-z0-9\+_\-]+)(\.[a-z0-9\+_\-]+)*@([a-z0-9\-]+\.)+[a-z]{2,6}\z/uiD';
+    if ( !preg_match( $pattern, $email ) ) {
+      $error['email'] = '*メールアドレスの形式が正しくありません。';
+    }
+  }
+  if ( $tel != '' && preg_match( '/\A\(?\d{2,5}\)?[-(\.\s]{0,2}\d{1,4}[-)\.\s]{0,2}\d{3,4}\z/u', $tel ) == 0 ) {
+    $error['tel'] = '*電話番号の形式が正しくありません。';
+  }
+  if ( $subject == '' ) {
+    $error['subject'] = '*件名は必須項目です。';
+    //制御文字でないことと文字数をチェック
+  } else if ( preg_match( '/\A[[:^cntrl:]]{1,50}\z/u', $subject ) == 0 ) {
+    $error['subject'] = '*件名は50文字以内でお願いします。';
+  }
+  if ( $body == '' ) {
+    $error['body'] = '*内容は必須項目です。';
+    //制御文字（タブ、復帰、改行を除く）でないことと文字数をチェック
+  } else if ( preg_match( '/\A[\r\n\t[:^cntrl:]]{1,300}\z/u', $body ) == 0 ) {
+    $error['body'] = '*内容は300文字以内でお願いします。';
+  }
+  
+  //エラーがなく且つ POST でのリクエストの場合
+  if (empty($error) && $_SERVER['REQUEST_METHOD']==='POST') {
+    //メールアドレス等を記述したファイルの読み込み
+    require 'mailvars.php'; 
+ 
+    //メール本文の組み立て
+    $mail_body = 'コンタクトページからのお問い合わせ' . "\n\n";
+    $mail_body .=  "お名前： " .h($name) . "\n";
+    $mail_body .=  "Email： " . h($email) . "\n"  ;
+    $mail_body .=  "お電話番号： " . h($tel) . "\n\n" ;
+    $mail_body .=  "＜お問い合わせ内容＞" . "\n" . h($body);
+ 
+    //--------sendmail------------
+ 
+    //メールの宛先（名前<メールアドレス> の形式）。値は mailvars.php に記載
+    $mailTo = mb_encode_mimeheader(MAIL_TO_NAME) ."<" . MAIL_TO. ">";
+ 
+    //Return-Pathに指定するメールアドレス
+    $returnMail = MAIL_RETURN_PATH; //
+    //mbstringの日本語設定
+    mb_language( 'ja' );
+    mb_internal_encoding( 'UTF-8' );
+ 
+    // 送信者情報（From ヘッダー）の設定
+    $header = "From: " . mb_encode_mimeheader($name) ."<" . $email. ">\n";
+    //$header .= "Cc: " . mb_encode_mimeheader(MAIL_CC_NAME) ."<" . MAIL_CC.">\n";
+    //$header .= "Bcc: <" . MAIL_BCC.">";
+ 
+    //メールの送信
+    //メールの送信結果を変数に代入
+    if ( ini_get( 'safe_mode' ) ) {
+      //セーフモードがOnの場合は第5引数が使えない
+      $result = mb_send_mail( $mailTo, $subject, $mail_body, $header );
+    } else {
+      $result = mb_send_mail( $mailTo, $subject, $mail_body, $header, '-f' . $returnMail );
+    }
+    
+    //メール送信の結果判定
+    if ( $result ) {
+      $_POST = array(); //空の配列を代入し、すべてのPOST変数を消去
+      //変数の値も初期化
+      $name = '';
+      $email = '';
+      $tel = '';
+      $subject = '';
+      $body = '';
+      $image = '';
+      
+      //再読み込みによる二重送信の防止
+      $params = '?result='. $result;
+      //サーバー変数 $_SERVER['HTTPS'] が取得出来ない環境用
+      if(isset($_SERVER['HTTP_X_FORWARDED_PROTO']) and $_SERVER['HTTP_X_FORWARDED_PROTO'] === "https") {
+        $_SERVER['HTTPS'] = 'on';
+      }
+      $url = (empty($_SERVER['HTTPS']) ? 'http://' : 'https://').$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME']; 
+      header('Location:' . $url . $params);
+      exit;
+    } 
+  }
+}
 ?>
 <!DOCTYPE html>
-<?php require_once("head.php")?>
-<title>貸し借り|検索</title>
-</head>
-<body>
-<audio id="audio"></audio>
-<div id="fb-root"></div>
-<script>(function(d, s, id) {
-  var js, fjs = d.getElementsByTagName(s)[0];
-  if (d.getElementById(id)) return;
-  js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.5&appId=643231655816289";
-  fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));</script>
-  
-  <!--ヘッダー-->
-		<?php require_once("header.php");?>
+<html lang="ja">
 
-<div>
-  <!-- 入力フォームを作る -->
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>貸し借りサイト　WACCA</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/original.css">
+  <script src="js/original.js">
+  </script>
+</head>
+
+<body id="page-top">
+
+  <!-- Page Wrapper -->
   <div id="wrapper">
-    <!--メイン-->
-    <div id="main">
-<section id="point">
-        <div>
+
+    <!-- Sidebar -->
+    <?php require_once("sidebar.php");?>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <?php require_once("nav.php");?>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">検索結果</h1>
+            <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                class="fas fa-download fa-sm text-white-50"></i> ダウンロードできません</a> -->
+          </div>
+
+          <div class="row">
+          <div>
         <?php
         $message = $_POST["message"];
         try{
@@ -94,33 +234,70 @@ if (empty($_POST)){
 </div>
       </section>
     </div>
-    <!--/メイン-->
+        </div>
+        <!-- /.container-fluid -->
 
-    <!--サイド-->
-    
-      <?php
-    require_once('side.php');
-    ?>
+      </div>
+      <!-- End of Main Content -->
 
-    
-    <!--/サイド-->
-  </div>
-  <!--/wrapper-->
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2021</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
 
-  <!--フッター-->
-  <footer>
-    <div id="footer_nav">
-    <ul>
-        <li class="current"><a href="index.php">HOME</a></li>
-        <li><a href="add_db.php">商品登録</a></li>
-        <li><a href="user_chat_list.php">一覧</a></li>
-        <li><a href="mypage.php">マイページ</a></li>
-        <li><a href="register.php">アカウント登録</a></li><li><a href="login.php">ログイン</a></li>
-      </ul>
     </div>
-    <small>&copy; 2015 Bloom.</small>
-  </footer>
-  <!--/フッター-->
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">本当にログアウトするのですね？</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">ログアウトしますか？</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">しない</button>
+          <a class="btn btn-danger" href="logout.php">ログアウト</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-demo.js"></script>
 
 </body>
+
 </html>
