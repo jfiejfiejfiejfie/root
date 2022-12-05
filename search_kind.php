244cb53906b9265d29a39c5ba8f338741a58dc0e
wakkasan <fki2166275@stu.o-hara.ac.jp>
2022-12-05 16:16:00 +0900

    検索とか

--- search_kind.php
+++ search_kind.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once('../lib/util.php');
-$myURL='search_kind.php';
+$myURL = 'search_kind.php';
 $gobackURL = "search_sp.php";
 require_once "db_connect.php";
 define('MAX', '5');
@@ -42,29 +42,64 @@ $myURL = 'search_kind.php';
 $option = "&kind_name=$kind_name";
 ?>
 <!DOCTYPE html>
-<?php require_once("head.php") ?>
-<title>貸し借り|検索</title>
+<html lang="ja">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>貸し借りサイト　WACCA</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/original.css">
+  <script src="js/original.js">
+  </script>
 </head>
 
-<body>
-  <audio id="audio"></audio>
-  <div id="fb-root"></div>
+<body id="page-top">
 
+  <!-- Page Wrapper -->
+  <div id="wrapper">
 
-  <!--ヘッダー-->
-  <?php require_once("header.php"); ?>
+    <!-- Sidebar -->
+    <?php require_once("sidebar.php"); ?>
+    <!-- End of Sidebar -->
 
-  <div>
-    <!-- 入力フォームを作る -->
-    <div id="wrapper">
-      <!--メイン-->
-      <div id="main">
-        <section id="point">
-          <h2>ジャンル検索</h2>
-          <form method="POST" action="search_kind.php">
-            <ul>
-              <li>
-                <label>ジャンルで検索します：<br>
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <?php require_once("nav.php"); ?>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">ジャンル検索</h1>
+            <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                class="fas fa-download fa-sm text-white-50"></i> ダウンロードできません</a> -->
+          </div>
+
+          <div class="row">
+            <form method="POST" action="search_kind.php">
+              <label>ジャンルで検索します：<br>
+                <div class="input-group">
                   <select name="kind_name">
                     <?php
                     try {
@@ -83,118 +118,153 @@ $option = "&kind_name=$kind_name";
                     }
                     ?>
                   </select>
-                </label>
-              </li>
-              <li><input type="submit" value="検索する"></li>
-            </ul>
-          </form>
-        </section>
-        <section id="point">
-          <h2>出品物一覧</h2>
-          <div>
-            <?php
-            try {
-              if (isset($_SESSION["loggedin"])) {
-                $block = 0;
-                require_once('block_check.php');
-                if ($block_count != 0) {
-                  $block_list = implode(",", $block_list);
-                  $sql = "SELECT * FROM list WHERE kind LIKE(:item) and loan=0 and user_id not in ($block_list)";
+                  <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit">
+                      <i class="fas fa-search fa-sm"></i>
+                    </button>
+                  </div>
+                </div>
+              </label>
+            </form>
+            <div class="col-12"></div>
+            <h2>出品物一覧</h2>
+            <div class="col-12">
+              <?php
+              try {
+                if (isset($_SESSION["loggedin"])) {
+                  $block = 0;
+                  require_once('block_check.php');
+                  if ($block_count != 0) {
+                    $block_list = implode(",", $block_list);
+                    $sql = "SELECT * FROM list WHERE kind LIKE(:item) and loan=0 and user_id not in ($block_list)";
+                  } else {
+                    $sql = "SELECT * FROM list WHERE kind LIKE(:item) and loan=0";
+                  }
                 } else {
                   $sql = "SELECT * FROM list WHERE kind LIKE(:item) and loan=0";
                 }
-              }else{
-                $sql = "SELECT * FROM list WHERE kind LIKE(:item) and loan=0";
-              }
-              $stm = $pdo->prepare($sql);
-              $stm->bindValue(':item', $kind_name, PDO::PARAM_STR);
-              $stm->execute();
-              $result = $stm->fetchAll(PDO::FETCH_ASSOC);
-              require_once("paging.php");
-              if (count($disp_data) > 0) {
-                echo "ジャンル:{$kind_name}";
-                // テーブルのタイトル行
-                echo '<table class="table table-striped">';
-                echo '<thead><tr>';
-                echo '<th>', '貸出者', '</th>';
-                echo '<th>', '貸出物', '</th>';
-                echo '<th>', 'ジャンル', '</th>';
-                echo '<th>', '金額', '</th>';
-                echo '<th>', '画像', '</th>';
-                if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-                  if ($_SESSION["admin"] == 1) {
-                    echo '<th>', '削除', '</th>';
-                  }
-                }
-                echo '</tr></thead>';
-                echo '<tbody>';
-                foreach ($disp_data as $row) {
-                  echo '<tr>';
-                  $user_id = $row["user_id"];
-                  $sql = "SELECT * FROM users WHERE id=$user_id";
-                  $stm = $pdo->prepare($sql);
-                  $stm->execute();
-                  $result2 = $stm->fetchAll(PDO::FETCH_ASSOC);
-                  foreach ($result2 as $row2) {
-                    echo '<td>', $row2["name"];
-                  }
-                  echo "<br><a target='_blank' href='profile.php?id={$row['user_id']}'><img id='image' height='100' width='100'src='my_image.php?id={$row['user_id']}'></a></td>";
-                  echo '<td>', $row['item'], '</td>';
-                  echo '<td>', $row['kind'], '</td>';
-                  echo '<td>￥', number_format($row['money']), '</td>';
-                  echo "<td><a target='_blank' href=detail.php?id={$row["id"]}>", '<img height="100" width="100" src="image.php?id=', $row['id'], '"></a></td>';
-                  if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-                    if ($_SESSION["admin"] == 1) {
-                      $row['id'] = rawurlencode($row['id']);
-                      echo "<td><a class = 'btn btn-primary' href=delete.php?id={$row["id"]}>", "消す", '</a></td>';
+                $stm = $pdo->prepare($sql);
+                $stm->bindValue(':item', $kind_name, PDO::PARAM_STR);
+                $stm->execute();
+                $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+                require_once("paging.php");
+                if (count($disp_data) > 0) {
+                  echo "ジャンル:{$kind_name}";
+                  // テーブルのタイトル行
+                  echo '<table class="table table-striped table-hover">';
+                  echo '<thead><tr>';
+                  echo '<th>', '貸出者', '</th>';
+                  echo '<th>', '貸出物', '</th>';
+                  echo '<th>', 'ジャンル', '</th>';
+                  echo '<th>', 'コメント', '</th>';
+                  echo '<th>', '金額', '</th>';
+                  echo '<th>', '画像', '</th>';
+                  echo '</tr></thead>';
+                  echo '<tbody>';
+                  foreach ($disp_data as $row) {
+                    echo '<tr><td>';
+                    echo "<a href='profile.php?id={$row['user_id']}'><img height='100' width='100'src='my_image.php?id={$row['user_id']}'></a><br>";
+                    $user_id = $row["user_id"];
+                    $sql = "SELECT * FROM users WHERE id=$user_id";
+                    $stm = $pdo->prepare($sql);
+                    $stm->execute();
+                    $result2 = $stm->fetchAll(PDO::FETCH_ASSOC);
+                    foreach ($result2 as $row2) {
+                      echo $row2["name"] . "</td>";
                     }
+                    echo '<td class="col-2">', $row['item'], '</td>';
+                    echo '<td>', $row['kind'], '</td>';
+                    $text = $row["comment"];
+                    $text = strip_tags($text);
+                    $limit = 265;
+                    if (mb_strlen($text) > $limit) {
+                      $title = mb_substr($text, 0, $limit);
+                      $text = $title . '･･･';
+                    }
+                    echo '<td class="col-4">', $text, '</td>';
+                    echo '<td>￥', number_format($row['money']), '</td>';
+                    echo "<td><a href=detail.php?id={$row["id"]}>", '<img height="200" width="200" src="image.php?id=', $row['id'], '"></a></td>';
+                    echo '</tr>';
                   }
-                  echo '</tr>';
+                  echo '</tbody>';
+                  echo '</table>';
+                } else {
+                  echo "名前に「{$kind_name}」は見つかりませんでした。<br>";
                 }
-                echo '</tbody>';
-                echo '</table>';
-              } else {
-                echo "名前に「{$kind_name}」は見つかりませんでした。<br>";
+              } catch (Exception $e) {
+                echo '<span class="error">エラーがありました。</span><br>';
+                echo $e->getMessage();
               }
-            } catch (Exception $e) {
-              echo '<span class="error">エラーがありました。</span><br>';
-              echo $e->getMessage();
-            }
-            require_once('paging2.php');
-            ?>
-            <hr>
-            <p><a href="<?php echo $gobackURL ?>">戻る</a></p>
+              require_once('paging2.php');
+              ?>
+              <hr>
+              <p><a href="<?php echo $gobackURL ?>">戻る</a></p>
+            </div>
           </div>
-        </section>
+
+        </div>
+        <!-- /.container-fluid -->
+
       </div>
-      <!--/メイン-->
+      <!-- End of Main Content -->
 
-      <!--サイド-->
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2021</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
 
-      <?php
-      require_once('side.php');
-      ?>
-
-
-      <!--/サイド-->
     </div>
-    <!--/wrapper-->
+    <!-- End of Content Wrapper -->
 
-    <!--フッター-->
-    <footer>
-      <div id="footer_nav">
-        <ul>
-          <li class="current"><a href="index.php">HOME</a></li>
-          <li><a href="add_db.php">商品登録</a></li>
-          <li><a href="user_chat_list.php">一覧</a></li>
-          <li><a href="mypage.php">マイページ</a></li>
-          <li><a href="register.php">アカウント登録</a></li>
-          <li><a href="login.php">ログイン</a></li>
-        </ul>
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">本当にログアウトするのですね？</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">ログアウトしますか？</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">しない</button>
+          <a class="btn btn-danger" href="logout.php">ログアウト</a>
+        </div>
       </div>
-      <small>&copy; 2015 Bloom.</small>
-    </footer>
-    <!--/フッター-->
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-demo.js"></script>
 
 </body>
 
--- search_user.php
+++ search_user.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 require_once('../lib/util.php');
-$myURL='search_user.php';
+$myURL = 'search_user.php';
 require_once "db_connect.php";
 $gobackURL = "user_chat_list.php";
 
@@ -19,112 +19,188 @@ if (empty($_POST)) {
   header("Location:{$gobackURL}");
   exit();
 }
-
-
 ?>
 <!DOCTYPE html>
-<?php require_once("head.php") ?>
-<title>貸し借り|検索</title>
+<html lang="ja">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>貸し借りサイト　WACCA</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/original.css">
+  <script src="js/original.js">
+  </script>
 </head>
 
-<body>
-  <audio id="audio"></audio>
-  <div id="fb-root"></div>
+<body id="page-top">
 
+  <!-- Page Wrapper -->
+  <div id="wrapper">
 
-  <!--ヘッダー-->
-  <?php require_once("header.php"); ?>
+    <!-- Sidebar -->
+    <?php require_once("sidebar.php"); ?>
+    <!-- End of Sidebar -->
 
-  <div>
-    <!-- 入力フォームを作る -->
-    <div id="wrapper">
-      <!--メイン-->
-      <div id="main">
-        <section id="point">
-          <form method="POST" action="search_user.php">
-            <ul>
-              <li>
-                <label>ユーザ名を検索します（部分一致）：<br>
-                  <input type="text" name="user_name" placeholder="名前を入れてください。"
-                    value="<?php echo htmlspecialchars($_POST["user_name"]); ?>">
-                </label>
-              </li>
-              <li><input type="submit" value="検索する"></li>
-            </ul>
-          </form>
-          <?php
-  $user_name = $_POST["user_name"];
-  //MySQLデータベースに接続する
-  try {
-    if(isset($_SESSION["loggedin"])){
-      $block = 0;
-      require_once('block_check.php');
-      if ($block_count != 0) {
-        $block_list = implode(",", $block_list);
-        $sql = "SELECT * FROM users WHERE name LIKE(:name) and id not in ($block_list)";
-      } else {
-        $sql = "SELECT * FROM users WHERE name LIKE(:name)";
-      }
-    }else{
-      $sql = "SELECT * FROM users WHERE name LIKE(:name)";
-    }
-    $stm = $pdo->prepare($sql);
-    $stm->bindValue(':name', "%{$user_name}%", PDO::PARAM_STR);
-    $stm->execute();
-    $result = $stm->fetchAll(PDO::FETCH_ASSOC);
-    if (count($result) > 0) {
-      echo "<script> final(); </script>";
-      echo "ユーザ名に「{$user_name}」が含まれているレコード";
-      // テーブルのタイトル行
-      echo '<table class="table table-striped">';
-      echo '<thead><tr>';
-      echo '<th>', 'ユーザ', '</th>';
-      echo '<th>', 'コメント', '</th>';
-      echo '</tr></thead>';
-      echo '<tbody>';
-      foreach ($result as $row) {
-        echo '<tr>';
-        echo '<td>',$row['name'];
-        echo "<br><a target='_blank' href='profile.php?id={$row['id']}'><img id='image' height='100' width='100'src='my_image.php?id={$row['id']}'></a></td>";
-        echo '<td>',$row['comment'];
-        echo '</tr>';
-      }
-      echo '</tbody>';
-      echo '</table>';
-    } else {
-      echo "名前に「{$user_name}」は見つかりませんでした。";
-      echo "<script> suteki(); </script>";
-    }
-  } catch (Exception $e) {
-    echo '<span class="error">エラーがありました。</span><br>';
-    echo $e->getMessage();
-  }
-  ?>
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <?php require_once("nav.php"); ?>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">ユーザ検索</h1>
+            <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                class="fas fa-download fa-sm text-white-50"></i> ダウンロードできません</a> -->
+          </div>
+
+          <div class="row">
+            <form method="POST" action="search_user.php">
+              <label>ユーザ名を検索します（部分一致）：<br>
+                  <div class="input-group">
+                      <input type="text" name="user_name" class="form-control form-control-user"
+                        placeholder="名前を入れてください。" value="<?php echo htmlspecialchars($_POST["user_name"]); ?>">
+                      <div class="input-group-append">
+                        <button class="btn btn-primary" type="submit">
+                          <i class="fas fa-search fa-sm"></i>
+                        </button>
+                      </div>
+                    </div>
+              </label>
+            </form>
+            <div class="col-12"></div>
+            <?php
+            $user_name = $_POST["user_name"];
+            //MySQLデータベースに接続する
+            try {
+              if (isset($_SESSION["loggedin"])) {
+                $block = 0;
+                require_once('block_check.php');
+                if ($block_count != 0) {
+                  $block_list = implode(",", $block_list);
+                  $sql = "SELECT * FROM users WHERE name LIKE(:name) and id not in ($block_list)";
+                } else {
+                  $sql = "SELECT * FROM users WHERE name LIKE(:name)";
+                }
+              } else {
+                $sql = "SELECT * FROM users WHERE name LIKE(:name)";
+              }
+              $stm = $pdo->prepare($sql);
+              $stm->bindValue(':name', "%{$user_name}%", PDO::PARAM_STR);
+              $stm->execute();
+              $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+              if (count($result) > 0) {
+                echo "ユーザ名に「{$user_name}」が含まれているレコード";
+                // テーブルのタイトル行
+                echo '<table class="table table-striped">';
+                echo '<thead><tr>';
+                echo '<th>', 'ユーザ', '</th>';
+                echo '<th>', 'コメント', '</th>';
+                echo '</tr></thead>';
+                echo '<tbody>';
+                foreach ($result as $row) {
+                  echo '<tr>';
+                  echo '<td>', $row['name'];
+                  echo "<br><a target='_blank' href='profile.php?id={$row['id']}'><img id='image' height='100' width='100'src='my_image.php?id={$row['id']}'></a></td>";
+                  echo '<td>', $row['comment'];
+                  echo '</tr>';
+                }
+                echo '</tbody>';
+                echo '</table>';
+              } else {
+                echo "名前に「{$user_name}」は見つかりませんでした。";
+              }
+            } catch (Exception $e) {
+              echo '<span class="error">エラーがありました。</span><br>';
+              echo $e->getMessage();
+            }
+            ?>
+          </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
       </div>
-      </section>
-      <!--/メイン-->
+      <!-- End of Main Content -->
 
-      <!--サイド-->
-      <?php require_once("side.php"); ?>
-      <!--/サイド-->
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2021</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
     </div>
-    <!--/wrapper-->
+    <!-- End of Content Wrapper -->
 
-    <!--フッター-->
-    <footer>
-      <div id="footer_nav">
-        <ul>
-          <li class="current"><a href="index.php">HOME</a></li>
-          <li><a href="add_db.php">商品登録</a></li>
-          <li><a href="user_chat_list.php">一覧</a></li>
-          <li><a href="mypage.php">マイページ</a></li>
-          <li><a href="register.php">アカウント登録</a></li>
-          <li><a href="login.php">ログイン</a></li>
-        </ul>
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">本当にログアウトするのですね？</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">ログアウトしますか？</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">しない</button>
+          <a class="btn btn-danger" href="logout.php">ログアウト</a>
+        </div>
       </div>
-      <small>&copy; 2015 Bloom.</small>
-    </footer>
-    <!--/フッター-->
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-demo.js"></script>
 
 </body>
 
