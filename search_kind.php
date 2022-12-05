52378a5573fa249d3975e0d22822bec6cde4e071
wakkasan <fki2166275@stu.o-hara.ac.jp>
2022-12-05 16:43:42 +0900

    ごめんなさい

--- search_kind.php
+++ search_kind.php
@@ -55,8 +55,7 @@ $option = "&kind_name=$kind_name";
   <title>貸し借りサイト　WACCA</title>
 
   <!-- Custom fonts for this template-->
-  <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
-  <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css"><link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
   <link
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet">
@@ -121,7 +120,6 @@ $option = "&kind_name=$kind_name";
                   </select>
                   <div class="input-group-append">
                     <button class="btn btn-primary" type="submit">
-                      <i class="fa fa-youtube-play" aria-hidden="true"></i>
                       <i class="fas fa-search fa-sm"></i>
                     </button>
                   </div>
