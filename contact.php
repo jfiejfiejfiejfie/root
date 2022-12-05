7f262226e62113005993945cc5579429540eab70
wakkasan <fki2166275@stu.o-hara.ac.jp>
2022-12-05 16:42:00 +0900

    大容量です

--- contact.php
+++ contact.php
@@ -143,7 +143,7 @@ if (isset($_POST['submitted'])) {
   <title>貸し借りサイト　WACCA</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
   <link
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet">
--- search_money.php
+++ search_money.php
@@ -65,7 +65,7 @@ $option = "&money1=$money1&money2=$money2";
   <title>貸し借りサイト　WACCA</title>
 
   <!-- Custom fonts for this template-->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css">
   <link
     href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
     rel="stylesheet">
