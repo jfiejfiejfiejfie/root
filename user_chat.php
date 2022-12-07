b87008860dec16807a60adf9c4c8807feea86969
wakkasan <fki2166275@stu.o-hara.ac.jp>
2022-12-07 14:26:32 +0900

    チャット完成

--- user_chat.php
+++ user_chat.php
@@ -1,4 +1,6 @@
 <?php
+use Vtiful\Kernel\Format;
+
 session_start();
 require_once('../lib/util.php');
 require_once "db_connect.php";
@@ -65,196 +67,282 @@ $stm = $pdo->prepare($sql);
 $stm->execute();
 $result = $stm->fetchAll(PDO::FETCH_ASSOC);
 foreach ($result as $row) {
-  $user_name = $row["name"];
+  $user_name2 = $row["name"];
 }
 ?>
-
 <!DOCTYPE html>
-<?php require_once("head.php") ?>
-<title>貸し借り|詳細</title>
+<html lang="ja">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>貸し借りサイト　WACCA</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/original.css">
+  <script src="js/original.js">
+  </script>
 </head>
 
-<body>
-  <audio id="audio"></audio>
-  <div id="fb-root"></div>
+<body id="page-top">
 
+  <!-- Page Wrapper -->
+  <div id="wrapper">
 
-  <!--ヘッダー-->
-  <?php require_once("header.php"); ?>
+    <!-- Sidebar -->
+    <?php require_once("sidebar.php"); ?>
+    <!-- End of Sidebar -->
 
-  <div>
-    <div id="wrapper">
-      <!--メイン-->
-      <div id="main">
-        <section id="point">
-          <h2>
-            <?php
-            echo $user_name;
-            ?>とのチャット履歴
-          </h2>
-          <?php
-          $block_count = 0;
-          $block_list = [];
-          $sql = "SELECT * FROM blocklist WHERE my_id =:id";
-          $stm = $pdo->prepare($sql);
-          $stm->bindValue(':id', $_SESSION["id"], PDO::PARAM_STR);
-          $stm->execute();
-          $block_result = $stm->fetchAll(PDO::FETCH_ASSOC);
-          foreach ($block_result as $block_row) {
-            $block_count += 1;
-            $block_list[] = $block_row["user_id"];
-          }
-          if (!in_array($id, $block_list)) {
-          ?>
-          <?php if (isset($_GET["page_id"])) {
-              echo '<form action="user_chat.php?id=' . $id . '&chat=1&page_id=' . $now . '" method="POST"enctype="multipart/form-data">';
-            } else {
-              echo '<form action="user_chat.php?id=' . $id . '&chat=1" method="POST"enctype="multipart/form-data">';
-            }
-          ?>
-          <label>画像選択:<br>
-            <img src="images/imageplus.png" id="preview" style="max-width:200px;"><br>
-            <input type="file" multiple name="image" class="test" accept="image/*" onchange="previewImage(this);">
-          </label><br>
-          チャット:<input type="text" name="text" required>
-          <br>
-          <!-- </div> -->
-          <input type="submit" value="送信">
-          </form>
-          <?php } else { ?>
-          <h2>ブロック中のため送信できません。</h2>
-          <?php } ?>
-          <hr>
-          <div>
-            <?php
-            $sql = "SELECT * FROM user_chat WHERE (others_id=$users_id and user_id=$id) or (others_id = $id  and user_id = $users_id) ORDER BY created_at DESC";
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        <?php require_once("nav.php"); ?>
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800">
+              <?php
+              echo $user_name2;
+              ?>とのチャット履歴
+            </h1>
+            <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                class="fas fa-download fa-sm text-white-50"></i> ダウンロードできません</a> -->
+          </div>
+
+          <div class="row">
+            <div class="col-12">
+              <?php
+            $id = $_GET["id"];
+            $block_count = 0;
+            $block_list = [];
+            $sql = "SELECT * FROM blocklist WHERE my_id =:id";
             $stm = $pdo->prepare($sql);
+            $stm->bindValue(':id', $_SESSION["id"], PDO::PARAM_STR);
             $stm->execute();
-            $result = $stm->fetchAll(PDO::FETCH_ASSOC);
-            require_once('paging.php');
-            foreach ($disp_data as $row) {
-              if (($row["others_id"] == $users_id)) {
-                if ($row["checked"] == 0) {
-                  $sql = "UPDATE user_chat SET checked=1 where id = " . $row["id"];
-                  $stm = $pdo->prepare($sql);
-                  $stm->execute();
-                }
-                echo '<table id="user_chat">';
-                echo '<thead><tr>';
-                echo '<th><a href="profile.php?id=', $row["user_id"], '">', '<img id="image" height="100" width="100" src="my_image.php?id=', $row["user_id"], '"></a>';
-                $user_id = $row["user_id"];
-                $sql = "SELECT * FROM users WHERE id=$user_id";
-                $stm = $pdo->prepare($sql);
-                $stm->execute();
-                $result2 = $stm->fetchAll(PDO::FETCH_ASSOC);
-                foreach ($result2 as $row2) {
-                  $block_list = [];
-                  $sql = "SELECT * FROM blocklist WHERE my_id =:id";
-                  $stm = $pdo->prepare($sql);
-                  $stm->bindValue(':id', $_SESSION["id"], PDO::PARAM_STR);
-                  $stm->execute();
-                  $block_result = $stm->fetchAll(PDO::FETCH_ASSOC);
-                  foreach ($block_result as $block_row) {
-                    $block_list[] = $block_row["user_id"];
-                  }
-                  echo '<br>';
-                  if (in_array($user_id, $block_list)) {
-                    echo $row2["name"], "<c style='color:red;'>※ブロック中!</c></th>";
-                  } else {
-                    echo $row2["name"], "</th>";
-                  }
-                }
-                $user_id = $row["user_id"];
-                $sql = "SELECT * FROM users WHERE id=$user_id";
-                $stm = $pdo->prepare($sql);
-                $stm->execute();
-                $result2 = $stm->fetchAll(PDO::FETCH_ASSOC);
-                foreach ($result2 as $row2) {
-                  echo "<td>";
-                  //整形したい文字列
-                  $text = $row["text"];
-                  echo $text;
-                  if ($row["image"] != "") {
-                    echo '<img "height="150" width="150" src="user_chat_image.php?id=' . $row["id"] . '"><br>';
-                    ;
-
-                  }
-                  echo '<br>', $row["created_at"];
-                  echo '</td>';
-                }
-                echo '</tr>';
-                echo '</thead>';
-                echo '</table>';
+            $block_result = $stm->fetchAll(PDO::FETCH_ASSOC);
+            foreach ($block_result as $block_row) {
+              $block_count += 1;
+              $block_list[] = $block_row["user_id"];
+            }
+            if (!in_array($id, $block_list)) {
+            ?>
+              <div>
+                <?php if (isset($_GET["page_id"])) {
+                echo '<form action="user_chat.php?id=' . $id . '&chat=1&page_id=' . $now . '" method="POST"enctype="multipart/form-data">';
               } else {
-                echo '<table id="user_chat">';
-                echo '<thead><tr>';
-                echo '<td>';
-                if ($row["image"] != "") {
-                  echo '<img "height="150" width="150" src="user_chat_image.php?id=' . $row["id"] . '"><br>';
-                }
-                $text = $row["text"];
-                echo $text;
-                echo '<br><br><div style="padding-left: 500px;">', $row["created_at"];
-                if($row["checked"]==1){
-                  echo ' 既読</div> ';
-                }
-                echo '</td>';
-                echo '<th><a href="profile.php?id=', $row["user_id"], '">', '<img id="image" height="100" width="100" src="my_image.php?id=', $row["user_id"], '"></a>';
-                $user_id = $row["user_id"];
-                $sql = "SELECT * FROM users WHERE id=$user_id";
-                $stm = $pdo->prepare($sql);
-                $stm->execute();
-                $result2 = $stm->fetchAll(PDO::FETCH_ASSOC);
-                foreach ($result2 as $row2) {
-                  $block_list = [];
-                  $sql = "SELECT * FROM blocklist WHERE my_id =:id";
-                  $stm = $pdo->prepare($sql);
-                  $stm->bindValue(':id', $_SESSION["id"], PDO::PARAM_STR);
-                  $stm->execute();
-                  $block_result = $stm->fetchAll(PDO::FETCH_ASSOC);
-                  foreach ($block_result as $block_row) {
-                    $block_list[] = $block_row["user_id"];
+                echo '<form action="user_chat.php?id=' . $id . '&chat=1" method="POST"enctype="multipart/form-data">';
+              }
+              ?>
+                <label>画像選択:<br>
+                  <img src="images/imageplus.png" id="preview" style="max-width:200px;"><br>
+                  <input type="file" multiple name="image" class="test" accept="image/*" onchange="previewImage(this);">
+                </label><br>
+                <div class="input-group col-12">
+                  <input type="text" name="text" class="form-control form-control-user" required>
+                  <!-- </div> -->
+                  <div class="input-group-append">
+                    <button type="submit" class="btn btn-primary"><i class="fa fa-paper-plane"
+                        aria-hidden="true"></i></button>
+                  </div>
+                </div>
+                </form>
+              </div>
+              <?php } else { ?>
+              <h2>ブロック中のため送信できません。</h2>
+              <?php } ?>
+              <hr>
+              <div>
+                <?php
+              $sql = "SELECT * FROM user_chat WHERE (others_id=$users_id and user_id=$id) or (others_id = $id  and user_id = $users_id) ORDER BY created_at DESC";
+              $stm = $pdo->prepare($sql);
+              $stm->execute();
+              $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+              require_once('paging.php');
+              foreach ($disp_data as $row) {
+                if (($row["others_id"] == $users_id)) {
+                  if ($row["checked"] == 0) {
+                    $sql = "UPDATE user_chat SET checked=1 where id = " . $row["id"];
+                    $stm = $pdo->prepare($sql);
+                    $stm->execute();
                   }
-                  echo '<br></th>';
+                  echo '<table id="user_chat">';
+                  echo '<thead><tr>';
+                  echo '<th><a href="profile.php?id=', $row["user_id"], '">', '<img id="image" height="100" width="100" src="my_image.php?id=', $row["user_id"], '"></a>';
+                  $user_id = $row["user_id"];
+                  $sql = "SELECT * FROM users WHERE id=$user_id";
+                  $stm = $pdo->prepare($sql);
+                  $stm->execute();
+                  $result2 = $stm->fetchAll(PDO::FETCH_ASSOC);
+                  foreach ($result2 as $row2) {
+                    $block_list = [];
+                    $sql = "SELECT * FROM blocklist WHERE my_id =:id";
+                    $stm = $pdo->prepare($sql);
+                    $stm->bindValue(':id', $_SESSION["id"], PDO::PARAM_STR);
+                    $stm->execute();
+                    $block_result = $stm->fetchAll(PDO::FETCH_ASSOC);
+                    foreach ($block_result as $block_row) {
+                      $block_list[] = $block_row["user_id"];
+                    }
+                    echo '<br>';
+                    if (in_array($user_id, $block_list)) {
+                      echo $row2["name"], "<c style='color:red;'>※ブロック中!</c></th>";
+                    } else {
+                      echo $row2["name"], "</th>";
+                    }
+                  }
+                  $user_id = $row["user_id"];
+                  $sql = "SELECT * FROM users WHERE id=$user_id";
+                  $stm = $pdo->prepare($sql);
+                  $stm->execute();
+                  $result2 = $stm->fetchAll(PDO::FETCH_ASSOC);
+                  foreach ($result2 as $row2) {
+                    echo "<td>";
+                    //整形したい文字列
+                    $text = $row["text"];
+                    if ($row["image"] != "") {
+                      echo '<img style="width:25%;" src="user_chat_image.php?id=' . $row["id"] . '"><br>';
+
+                    }
+                    echo '<div style="font-size:35px; font-family: serif;" class="balloon1">' . $text . '</div>';
+                    $time = new DateTime($row["created_at"]);
+                    $time = $time->format("H:i");
+                    echo '<div style="font-size:20px;">' . $time;
+                    echo '</div>';
+                    echo '</td>';
+                  }
                   echo '</tr>';
                   echo '</thead>';
                   echo '</table>';
+                } else {
+                  echo '<table id="user_chat">';
+                  echo '<thead><tr>';
+                  echo '<td>';
+                  // echo '<div class="clearfix">';
+                  if ($row["image"] != "") {
+                    echo '<div style="text-align: right;"><img style="width:25%;" src="user_chat_image.php?id=' . $row["id"] . '"></div><br>';
+                  }
+                  $text = $row["text"];
+                  echo '<div style="text-align: right; font-size:35px; font-family: serif;" class="balloon2 float-right">' . $text . '</div>';
+                  $time = new DateTime($row["created_at"]);
+                  $time = $time->format("H:i");
+                  echo '<div style="font-size:20px;" class="float-right mt-5">' . $time;
+                  if ($row["checked"] == 1) {
+                    echo ' 既読';
+                  }
+                  echo '</div>';
+                  echo '</td>';
+                  echo '<th><a href="profile.php?id=', $row["user_id"], '">', '<img id="image" height="150" width="150" src="my_image.php?id=', $row["user_id"], '"></a>';
+                  $user_id = $row["user_id"];
+                  $sql = "SELECT * FROM users WHERE id=$user_id";
+                  $stm = $pdo->prepare($sql);
+                  $stm->execute();
+                  $result2 = $stm->fetchAll(PDO::FETCH_ASSOC);
+                  foreach ($result2 as $row2) {
+                    $block_list = [];
+                    $sql = "SELECT * FROM blocklist WHERE my_id =:id";
+                    $stm = $pdo->prepare($sql);
+                    $stm->bindValue(':id', $_SESSION["id"], PDO::PARAM_STR);
+                    $stm->execute();
+                    $block_result = $stm->fetchAll(PDO::FETCH_ASSOC);
+                    foreach ($block_result as $block_row) {
+                      $block_list[] = $block_row["user_id"];
+                    }
+                    echo '<br></th>';
+                    echo '</tr>';
+                    echo '</thead>';
+                    echo '</table>';
+                  }
                 }
               }
-            }
-            require_once('paging2.php');
-            ?>
-            <p><a href="<?php echo $gobackURL ?>">戻る</a></p>
+              require_once('paging2.php');
+              ?>
+              </div>
+              <p><a href="<?php echo $gobackURL ?>">戻る</a></p>
+            </div>
+
           </div>
+          <!-- /.container-fluid -->
+
+        </div>
+        <!-- End of Main Content -->
+
+        <!-- Footer -->
+        <footer class="sticky-footer bg-white">
+          <div class="container my-auto">
+            <div class="copyright text-center my-auto">
+              <span>Copyright &copy; Your Website 2021</span>
+            </div>
+          </div>
+        </footer>
+        <!-- End of Footer -->
+
       </div>
-      <!--/メイン-->
+      <!-- End of Content Wrapper -->
 
-      <!--サイド-->
-      <?php
-      require_once('side.php');
-      ?>
-
-
-
-      <!--/サイド-->
     </div>
-    <!--/wrapper-->
+    <!-- End of Page Wrapper -->
 
-    <!--フッター-->
-    <footer>
-      <div id="footer_nav">
-        <ul>
-          <li class="current"><a href="index.php">HOME</a></li>
-          <li><a href="add_db.php">商品登録</a></li>
-          <li><a href="user_chat_list.php">一覧</a></li>
-          <li><a href="mypage.php">マイページ</a></li>
-          <li><a href="register.php">アカウント登録</a></li>
-          <li><a href="login.php">ログイン</a></li>
-        </ul>
+    <!-- Scroll to Top Button-->
+    <a class="scroll-to-top rounded" href="#page-top">
+      <i class="fas fa-angle-up"></i>
+    </a>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+      aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">本当にログアウトするのですね？</h5>
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">×</span>
+            </button>
+          </div>
+          <div class="modal-body">ログアウトしますか？</div>
+          <div class="modal-footer">
+            <button class="btn btn-secondary" type="button" data-dismiss="modal">しない</button>
+            <a class="btn btn-danger" href="logout.php">ログアウト</a>
+          </div>
+        </div>
       </div>
-      <small>&copy; 2015 Bloom.</small>
-    </footer>
-    <!--/フッター-->
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="js/demo/chart-area-demo.js"></script>
+    <script src="js/demo/chart-pie-demo.js"></script>
 
 </body>
 
