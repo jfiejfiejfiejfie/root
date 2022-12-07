6c77af9d8ca7706aed2beb20ab613a5ea7234457
jfiejfiejfiejfie <yuttarishin@gmail.com>
2022-12-07 14:35:28 +0900

    自分の商品の編集

--- detail1.php
+++ detail1.php
@@ -5,34 +5,46 @@ $myURL='detail1.php';
 $gobackURL = "my_edit.php?id={$_POST["id"]}";
 ?>
 <!DOCTYPE html>
-<html lang="ja" xmlns:og="http://ogp.me/ns#" xmlns:fb="https://www.faceboook.com/2008/fbml">
+<html lang="ja">
 
-<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
-  <meta charset="UTF-8">
-  <meta property="og:title" content="フラワーアレンジメント教室　Bloom【ブルーム】">
-  <meta property="og:description" content="東京都千代田区にあるフラワーアレンジメント教室Bloom【ブルーム】">
-  <meta property="og:url" content="http://bloom.ne.jp">
-  <meta property="og:image" content="images/main_visual.jpg">
-  <title>貸し借り</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description"
-    content="東京都千代田区にあるフラワーアレンジメント教室Bloom【ブルーム】。一人ひとりに向き合った、その人らしいアレンジメントを考えながら楽しく学べます。初心者の方も安心してご参加ください。">
-  <link rel="stylesheet" href="css/styled.css">
-  <link rel="stylesheet" href="css/font-awesome.min.css">
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>貸し借りサイト　WACCA</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
   <link rel="stylesheet" href="css/original.css">
-  <link rel="stylesheet" href="css/common.css">
-  <link rel="stylesheet" href="css/l.css">
-  <link rel="stylesheet" href="css/m.css">
-  <link rel="stylesheet" href="css/s.css">
-  <link rel="favicon.ico">
-  <link rel="apple-touch-icon" href="webclip152.png">
   <script src="js/original.js">
   </script>
 </head>
 
-<body>
-  <audio id="audio"></audio>
-  <?php
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+    <?php require_once("sidebar.php");?>
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+      <?php
 // 簡単なエラー処理
 $errors = [];
 if (!isset($_POST["money"]) || (!ctype_digit($_POST["money"]))) {
@@ -56,16 +68,22 @@ if (count($errors) > 0) {
 }
 require_once "db_connect.php";
 ?>
-  <div id="fb-root"></div>
+        <!-- Topbar -->
+        <?php require_once("nav.php");?>
+        <!-- End of Topbar -->
 
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
 
-  <!--ヘッダー-->
-  <?php require_once("header.php"); ?>
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h3 mb-0 text-gray-800"></h1>
+            <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
+                                class="fas fa-download fa-sm text-white-50"></i> ダウンロードできません</a> -->
+          </div>
 
-  <div id="wrapper">
-    <!--メイン-->
-    <div id="main">
-      <h1>変更完了しますた。</h1>
+          <div class="row">
+          <h1>変更完了しました。</h1>
       <?php
   date_default_timezone_set('Asia/Tokyo');
   $created_at = date("Y/m/d H:i:s");
@@ -137,41 +155,80 @@ require_once "db_connect.php";
     exit();
   }
   ?>
-      <hr>
       <p><a href="<?php echo $gobackURL ?>">戻る</a></p>
-      <!--/メイン-->
+          </div>
+
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2021</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
     </div>
-    <!--サイド-->
+    <!-- End of Content Wrapper -->
 
-    <?php
-      require_once('side.php');
-      ?>
-
-
-    <!--/サイド-->
   </div>
-  <!--/wrapper-->
+  <!-- End of Page Wrapper -->
 
-  <!--フッター-->
-  <footer>
-    <div id="footer_nav">
-      <ul>
-        <li class="current"><a href="index.php">HOME</a></li>
-        <li><a href="add_db.php">商品登録</a></li>
-        <li><a href="user_chat_list.php">一覧</a></li>
-        <li><a href="mypage.php">マイページ</a></li>
-        <li>
-          <?php if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) { ?><a href="contact.php">お問い合わせ💛</a>
-          <?php } else { ?><a href="register.php">アカウント登録</a>
-          <?php } ?>
-        </li>
-        <li><a href="login.php">ログイン</a></li>
-      </ul>
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+    aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">本当にログアウトするのですね？</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body">ログアウトしますか？</div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">しない</button>
+          <a class="btn btn-danger" href="logout.php">ログアウト</a>
+        </div>
+      </div>
     </div>
-    <small>&copy; 2015 Bloom.</small>
-  </footer>
-  <!--/フッター-->
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+  <!-- Page level plugins -->
+  <script src="vendor/chart.js/Chart.min.js"></script>
+
+  <!-- Page level custom scripts -->
+  <script src="js/demo/chart-area-demo.js"></script>
+  <script src="js/demo/chart-pie-demo.js"></script>
 
 </body>
 
 </html>
+
+
+
+
+
+
+
