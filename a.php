3a978043e2a407fa184e0345a45cec9c4e200b5f
wakkasan <fki2166275@stu.o-hara.ac.jp>
2022-12-08 12:02:46 +0900

    削除

--- a.php
+++
@@ -1,6 +0,0 @@
-<?php
-$lines=file("named1.txt",FILE_IGNORE_NEW_LINES);
-$lines2=file("named2.txt",FILE_IGNORE_NEW_LINES);
-$new_lines="{$lines[rand(0,4)]}{$lines2[rand(0,2)]}";
-echo $new_lines;
-?>
