d659397096de8887295e9f5576673037c9fd9a17
wakkasan <fki2166275@stu.o-hara.ac.jp>
2022-12-09 11:38:07 +0900

    編集

--- edit.php
+++ edit.php
@@ -1,7 +1,6 @@
-
 <?php
 session_start();
-$myURL='edit.php';
+$myURL = 'edit.php';
 $gobackURL = 'mypage.php';
 require_once "db_connect.php";
 ?>
@@ -37,7 +36,7 @@ require_once "db_connect.php";
   <div id="wrapper">
 
     <!-- Sidebar -->
-    <?php require_once("sidebar.php");?>
+    <?php require_once("sidebar.php"); ?>
     <!-- End of Sidebar -->
 
     <!-- Content Wrapper -->
@@ -47,7 +46,7 @@ require_once "db_connect.php";
       <div id="content">
 
         <!-- Topbar -->
-        <?php require_once("nav.php");?>
+        <?php require_once("nav.php"); ?>
         <!-- End of Topbar -->
 
         <!-- Begin Page Content -->
@@ -59,64 +58,51 @@ require_once "db_connect.php";
             <!-- <a href="#" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                                 class="fas fa-download fa-sm text-white-50"></i> ダウンロードできません</a> -->
           </div>
-          <form method="POST" action="edit_db.php" enctype="multipart/form-data">
-        <?php
-              $id=$_SESSION["id"];
-              require_once('user_check.php');
-              echo "<img src=my_image.php?id=$id style='max-width:200px;'>";
-              echo "<hr>";
-          ?>
-        <ul>
-          <li>
-            <label>名前:
-              <input type="text" name="name" placeholder="名前" value="<?php echo htmlspecialchars($row["name"]); ?>"
-                required>
-            </label>
-          </li>
-          </li>
-            <label>年齢:
-              <input type="number" name="age" placeholder="年齢" value="<?php echo htmlspecialchars($row["age"]); ?>"
-                required>
-            </label>
-          </li>
-          <li>性別:
-            <label><input type="radio" name="sex" value="男" checked>男性</label>
-            <label><input type="radio" name="sex" value="女">女性</label>
-            <label><input type="radio" name="sex" value="無回答">無回答</label>
-          </li>
-          <?php if($row["checked"]==0){?>
-          <li>
-            <label>E-mail:
-              <input type="text" name="email" placeholder="E-mail"
-                value="<?php echo htmlspecialchars($row["email"]); ?>">
-            </label>
-          </li>
-          <?php }?>
-          <li>
-            <label>コメント:
-              <input type="text" name="comment" placeholder="comment"
-                value="<?php echo htmlspecialchars($row["comment"]); ?>" required>
-            </label>
-          </li>
-          <li>
-            <label>画像選択:<br>
-              <img src="images/imageplus.png" id="preview" style="max-width:200px;"><br>
-              <input type="file" multiple name="image" class="test" accept="image/*" onchange="previewImage(this);">
-            </label>
-          </li>
-          <li><input type="submit" value="変更する"></li>
-        </ul>
-      </form>
-      <hr>
-      <p><a href="<?php echo $gobackURL ?>">戻る</a></p>
-      <br>
-      <form method="POST" action="delete_conf.php" enctype="multipart/form-data">
-        <ul>
-          <li><input type="submit" value="退会する"></li>
-        </ul>
-      </form>
-          <div class="row">
 
+          <div class="row">
+            <div class="col-12">
+              <form method="POST" action="edit_db.php" enctype="multipart/form-data">
+                <?php
+                $id = $_SESSION["id"];
+                require_once('user_check.php');
+                echo "<img src=my_image.php?id=$id style='max-width:200px;'>";
+                echo "<hr>";
+                ?>
+                名前:
+                <input type="text" class="form-control form-control-user col-2" name="name" placeholder="名前"
+                  value="<?php echo htmlspecialchars($row["name"]); ?>" required>
+                年齢:
+                  <input type="number" name="age" class="form-control form-control-user col-1" placeholder="年齢"
+                    value="<?php echo htmlspecialchars($row["age"]); ?>" required>
+                <div>性別:
+                  <label><input type="radio" name="sex" value="男" checked>男性</label>
+                  <label><input type="radio" name="sex" value="女">女性</label>
+                  <label><input type="radio" name="sex" value="無回答">無回答</label>
+                </div>
+                <?php if ($row["checked"] == 0) { ?>
+                <div>E-mail:
+                  <input type="text" name="email" placeholder="E-mail" class="form-control form-control-user"
+                    value="<?php echo htmlspecialchars($row["email"]); ?>">
+                </div>
+                <?php } ?>
+                <div>コメント:
+                  <input type="text" name="comment" placeholder="comment" class="form-control form-control-user col-4"
+                    value="<?php echo htmlspecialchars($row["comment"]); ?>" required>
+                </div>
+                <label>画像選択:<br>
+                  <img src="images/imageplus.png" id="preview" style="max-width:200px;"><br>
+                  <input type="file" multiple name="image" class="test" accept="image/*" onchange="previewImage(this);">
+                </label>
+                <br>
+                <button type="submit" class="btn btn-primary col-1">編集</button>
+              </form>
+              <hr>
+              <a class="btn btn-primary" href="<?php echo $gobackURL ?>">戻る</a>
+              <div class="col-12"><br></div>
+              <form method="POST" action="delete_conf.php" enctype="multipart/form-data">
+                <button type="submit" class="btn btn-primary">退会する</button>
+              </form>
+            </div>
           </div>
 
         </div>
@@ -186,4 +172,3 @@ require_once "db_connect.php";
 </body>
 
 </html>
-
