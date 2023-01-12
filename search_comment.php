1ad5cc3aae9108eaa8691a1756056e6942ca5d56
wakka <fki2166301@stu.o-hara.ac.jp>
2023-01-12 15:22:27 +0900

    test

--- search_comment.php
+++ search_comment.php
@@ -28,7 +28,7 @@ if (isset($_POST["kind"])) {
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>貸し借りサイト　WACCA</title>
+  <title>貸し借りサイト　WACCAA</title>
 
   <!-- Custom fonts for this template-->
   <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
