c359e4f891667bbd2c4bd49b17b4ae805596637a
wakkasan <fki2166275@stu.o-hara.ac.jp>
2023-02-07 11:25:22 +0900

    あ

--- nav.php
+++ nav.php
@@ -329,7 +329,7 @@
         </a>
         <div class="dropdown-divider"></div>
         <a class="dropdown-item" href="logout.php" data-toggle="modal" data-target="#karasawa">
-          <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+          <i class="fas fa-asterisk fa-sm fa-fw mr-2 text-gray-400"></i>
           うんちを漏らす
         </a>
       </div>
