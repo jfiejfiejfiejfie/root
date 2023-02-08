932c578314805ce23b67d90acd3549d83e99ef0e
wakkasan <fki2166275@stu.o-hara.ac.jp>
2023-02-08 10:20:44 +0900

    修正致しました

--- detail.php
+++ detail.php
@@ -333,7 +333,7 @@ if (isset($_SESSION["id"])) {
     <!-- End of Page Wrapper -->
     <div class="modal fade" id="chat" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
       aria-hidden="true">
-      <div class="modal-dialog" role="document">
+      <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content">
           <div class="modal-header">
             <h5 class="modal-title" id="exampleModalLabel">
--- room.php
+++ room.php
@@ -409,7 +409,7 @@ if (isset($_GET["chat"])) {
   </div>
   <!-- End of Page Wrapper -->
   <div class="modal fade" id="stamp" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
+    <div class="modal-dialog modal-lg" role="document">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">
