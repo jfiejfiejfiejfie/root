f19c8eff8d4075bc4f948cda506154151239d137
jfiejfiejfiejfie <yuttarishin@gmail.com>
2023-02-14 11:25:17 +0900

    すみませんでした。

--- insert.php
+++ insert.php
@@ -1,37 +1,9 @@
 <?php
-  // 簡単なエラー処理
-  $errors = [];
-  // $money = $_POST["money"];
-  if((!isset($_POST["item"]))){
-    $errors[]="名前入れろカス";
-  }
-  if((!isset($_FILES["image"])) || ($_FILES["image"]["tmp_name"] == '')){
-    $errors[]="画像入れろカス";
-  }
-  // if (!isset($_POST["money"]) || (!ctype_digit($_POST["money"]))) {
-  //   $money = 100;
-  // }
-  // if ($money < 100 || 10000000 <= $money) {
-  //   $errors[] = "金額を100円以上10,000,000未満にしてください。";
-  // }
-  require_once('user_check.php');
-  // if($row["checked"]==0){
-  //   $errors[] = "メール認証をしてください。";
-  // }
-  //エラーがあったとき
-  if (count($errors) > 0) {
-    echo "<script> rikki(); </script>";
-    echo "<img src='images/main_visual.jpg'>";
-    echo '<ol class="error">';
-    foreach ($errors as $value) {
-      echo "<li>", $value, "</li>";
-    }
-    echo "</ol>";
-    echo "<hr>";
-    echo "<a href=", $gobackURL, ">戻る</a><br>";
-    $_SESSION["insert_text"]="";
-    exit();
-  }
+
+require_once('user_check.php');
+if ((!isset($_FILES["image"])) || ($_FILES["image"]["tmp_name"] == '')) {
+  header('Location:add_db.php?result=1');
+} else {
   $kind_id = $_POST["kind"];
   try {
     $sql = "SELECT * FROM kind WHERE id=$kind_id";
@@ -62,85 +34,84 @@
   }
   ?>
   <?php
-    $id = $_SESSION["id"];
-    date_default_timezone_set('Asia/Tokyo');
-    $created_at = date("Y/m/d H:i:s");
-    $item = $_POST["item"];
-    $comment = $_POST["comment"];
-    $upfile = $_FILES["image"]["tmp_name"];
-    $imgdat = file_get_contents($upfile);
-    try {
+  $id = $_SESSION["id"];
+  date_default_timezone_set('Asia/Tokyo');
+  $created_at = date("Y/m/d H:i:s");
+  $item = $_POST["item"];
+  $comment = $_POST["comment"];
+  $upfile = $_FILES["image"]["tmp_name"];
+  $imgdat = file_get_contents($upfile);
+  try {
 
 
-      $sql = "INSERT INTO list (created_at,user_id,item,comment,kind,image,state) VALUES(:created_at,:id,:item,:comment,:kind,:imgdat,:state)";
+    $sql = "INSERT INTO list (created_at,user_id,item,comment,kind,image,state) VALUES(:created_at,:id,:item,:comment,:kind,:imgdat,:state)";
+    $stm = $pdo->prepare($sql);
+    $stm->bindValue(':created_at', $created_at, PDO::PARAM_STR);
+    $stm->bindValue(':id', $id, PDO::PARAM_STR);
+    $stm->bindValue(':item', $item, PDO::PARAM_STR);
+    $stm->bindValue(':comment', $comment, PDO::PARAM_STR);
+    // $stm->bindValue(':money', $money, PDO::PARAM_STR);
+    $stm->bindValue(':kind', $kind_name, PDO::PARAM_STR);
+    $stm->bindValue(':imgdat', $imgdat, PDO::PARAM_STR);
+    $stm->bindValue(':state', $state_name, PDO::PARAM_STR);
+    if ($stm->execute()) {
+      $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+      $sql = 'SELECT * FROM list WHERE created_at = :created_at';
       $stm = $pdo->prepare($sql);
       $stm->bindValue(':created_at', $created_at, PDO::PARAM_STR);
-      $stm->bindValue(':id', $id, PDO::PARAM_STR);
-      $stm->bindValue(':item', $item, PDO::PARAM_STR);
-      $stm->bindValue(':comment', $comment, PDO::PARAM_STR);
-      // $stm->bindValue(':money', $money, PDO::PARAM_STR);
-      $stm->bindValue(':kind', $kind_name, PDO::PARAM_STR);
-      $stm->bindValue(':imgdat', $imgdat, PDO::PARAM_STR);
-      $stm->bindValue(':state', $state_name, PDO::PARAM_STR);
-      if ($stm->execute()) {
-        $result = $stm->fetchAll(PDO::FETCH_ASSOC);
-        $sql = 'SELECT * FROM list WHERE created_at = :created_at';
-        $stm = $pdo->prepare($sql);
-        $stm->bindValue(':created_at', $created_at, PDO::PARAM_STR);
-        $stm->execute();
-        $result = $stm->fetchAll(PDO::FETCH_ASSOC);
-        foreach ($result as $row) {
-          $list_id = $row["id"];
-        }
-        if (isset($_FILES["image2"]) && ($_FILES["image2"]["tmp_name"] != '')) {
-          $upfile = $_FILES["image2"]["tmp_name"];
-          $imgdat = file_get_contents($upfile);
-          $sql = "INSERT INTO image_list (list_id,image,number) VALUES(:list_id,:imgdat,:number)";
-          $stm = $pdo->prepare($sql);
-          $stm->bindValue(':list_id', $list_id, PDO::PARAM_STR);
-          $stm->bindValue(':imgdat', $imgdat, PDO::PARAM_STR);
-          $stm->bindValue(':number', 1, PDO::PARAM_STR);
-          $stm->execute();
-        }
-        if (isset($_FILES["image3"]) && ($_FILES["image3"]["tmp_name"] != '')) {
-          $upfile = $_FILES["image3"]["tmp_name"];
-          $imgdat = file_get_contents($upfile);
-          $sql = "INSERT INTO image_list (list_id,image,number) VALUES(:list_id,:imgdat,:number)";
-          $stm = $pdo->prepare($sql);
-          $stm->bindValue(':list_id', $list_id, PDO::PARAM_STR);
-          $stm->bindValue(':imgdat', $imgdat, PDO::PARAM_STR);
-          $stm->bindValue(':number', 2, PDO::PARAM_STR);
-          $stm->execute();
-        }
-        if (isset($_FILES["image4"]) && ($_FILES["image4"]["tmp_name"] != '')) {
-          $upfile = $_FILES["image4"]["tmp_name"];
-          $imgdat = file_get_contents($upfile);
-          $sql = "INSERT INTO image_list (list_id,image,number) VALUES(:list_id,:imgdat,:number)";
-          $stm = $pdo->prepare($sql);
-          $stm->bindValue(':list_id', $list_id, PDO::PARAM_STR);
-          $stm->bindValue(':imgdat', $imgdat, PDO::PARAM_STR);
-          $stm->bindValue(':number', 3, PDO::PARAM_STR);
-          $stm->execute();
-        }
-        if (isset($_FILES["image5"]) && ($_FILES["image5"]["tmp_name"] != '')) {
-          $upfile = $_FILES["image5"]["tmp_name"];
-          $imgdat = file_get_contents($upfile);
-          $sql = "INSERT INTO image_list (list_id,image,number) VALUES(:list_id,:imgdat,:number)";
-          $stm = $pdo->prepare($sql);
-          $stm->bindValue(':list_id', $list_id, PDO::PARAM_STR);
-          $stm->bindValue(':imgdat', $imgdat, PDO::PARAM_STR);
-          $stm->bindValue(':number', 4, PDO::PARAM_STR);
-          $stm->execute();
-        }
-        $_SESSION["insert_text"]="登録完了しました。";
-      } else {
-        echo "ツイカエラーガアリマシタ。";
-        $_SESSION["insert_text"]="";
+      $stm->execute();
+      $result = $stm->fetchAll(PDO::FETCH_ASSOC);
+      foreach ($result as $row) {
+        $list_id = $row["id"];
       }
-    } catch (Exception $e) {
-      echo 'エラーがありました。';
-      echo $e->getMessage();
-      exit();
+      if (isset($_FILES["image2"]) && ($_FILES["image2"]["tmp_name"] != '')) {
+        $upfile = $_FILES["image2"]["tmp_name"];
+        $imgdat = file_get_contents($upfile);
+        $sql = "INSERT INTO image_list (list_id,image,number) VALUES(:list_id,:imgdat,:number)";
+        $stm = $pdo->prepare($sql);
+        $stm->bindValue(':list_id', $list_id, PDO::PARAM_STR);
+        $stm->bindValue(':imgdat', $imgdat, PDO::PARAM_STR);
+        $stm->bindValue(':number', 1, PDO::PARAM_STR);
+        $stm->execute();
+      }
+      if (isset($_FILES["image3"]) && ($_FILES["image3"]["tmp_name"] != '')) {
+        $upfile = $_FILES["image3"]["tmp_name"];
+        $imgdat = file_get_contents($upfile);
+        $sql = "INSERT INTO image_list (list_id,image,number) VALUES(:list_id,:imgdat,:number)";
+        $stm = $pdo->prepare($sql);
+        $stm->bindValue(':list_id', $list_id, PDO::PARAM_STR);
+        $stm->bindValue(':imgdat', $imgdat, PDO::PARAM_STR);
+        $stm->bindValue(':number', 2, PDO::PARAM_STR);
+        $stm->execute();
+      }
+      if (isset($_FILES["image4"]) && ($_FILES["image4"]["tmp_name"] != '')) {
+        $upfile = $_FILES["image4"]["tmp_name"];
+        $imgdat = file_get_contents($upfile);
+        $sql = "INSERT INTO image_list (list_id,image,number) VALUES(:list_id,:imgdat,:number)";
+        $stm = $pdo->prepare($sql);
+        $stm->bindValue(':list_id', $list_id, PDO::PARAM_STR);
+        $stm->bindValue(':imgdat', $imgdat, PDO::PARAM_STR);
+        $stm->bindValue(':number', 3, PDO::PARAM_STR);
+        $stm->execute();
+      }
+      if (isset($_FILES["image5"]) && ($_FILES["image5"]["tmp_name"] != '')) {
+        $upfile = $_FILES["image5"]["tmp_name"];
+        $imgdat = file_get_contents($upfile);
+        $sql = "INSERT INTO image_list (list_id,image,number) VALUES(:list_id,:imgdat,:number)";
+        $stm = $pdo->prepare($sql);
+        $stm->bindValue(':list_id', $list_id, PDO::PARAM_STR);
+        $stm->bindValue(':imgdat', $imgdat, PDO::PARAM_STR);
+        $stm->bindValue(':number', 4, PDO::PARAM_STR);
+        $stm->execute();
+      }
+      header('Location:add_db.php?result=0');
+    } else {
+      echo "ツイカエラーガアリマシタ。";
     }
-    header('Location:add_db.php');
-    ?>
+  } catch (Exception $e) {
+    echo 'エラーがありました。';
+    echo $e->getMessage();
+    exit();
+  }
+}
+?>
